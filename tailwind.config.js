f1619a90371f1e597e272cda8ee945082b87d6d3
Francisco Daniel <francisco.daniel.108@ufrn.edu.br>
2025-07-31 10:58:12 -0300

    chore: setup inicial do projeto com Angular, Tailwind, PrimeNG e Keycloak

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,ts}",  // Olha todos arquivos HTML e TS dentro da pasta src
+  ],
+  theme: {
+    extend: {},  // Aqui você pode personalizar temas (cores, fontes, etc)
+  },
+  plugins: [],  // Plugins opcionais do Tailwind
+};
